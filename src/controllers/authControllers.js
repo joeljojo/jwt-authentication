0b384e6dcc862c13f3581cfac43252d852edb9f6
joeljojo <joelpamphyl@gmail.com>
2023-06-15 18:07:40 +0300

    Generated jwt token

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const ClientError = require("../exceptions/clientError");
 const { getUserByEmail, isPasswordCorrect } = require("../models/helper/index");
 const UnauthorizedError = require("../exceptions/unauthorizedError");
+const config = require("../config");
 
 const login = async (req, res, next) => {
   //Ensure email and password is provided
@@ -17,7 +18,15 @@ const login = async (req, res, next) => {
     throw new UnauthorizedError("Invalid email or password");
 
   //Generate and sign jwt token
-  res.status(200).json({ message: "Login Successfully" });
+  const token = jwt.sign({ user }, config.jwt.secret, {
+    expiresIn: "1h",
+    notBefore: "0", // Cannot use before now
+    algorithm: "HS256",
+  });
+
+  // This line of code shall not be hit when an error occurs
+  // return token in response
+  res.status(200).json({ message: "Login Successfully", token: token });
 };
 
 module.exports = { login };
