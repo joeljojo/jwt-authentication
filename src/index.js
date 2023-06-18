9c701fd192c96888b7bdacde7265520d47c4a124
joeljojo <joelpamphyl@gmail.com>
2023-06-18 16:20:42 +0300

    Added default user and Admin roles to the database

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ const errorHandler = require("./middlewares/errorHandler");
 // instatntiate express object.
 const app = express();
 
+const Role = db.role;
 app.use(express.json());
 
 //database
-db.sequelize.sync();
+db.sequelize.sync({ force: false }).then(() => {
+  // define default roles
+  createRoles();
+});
 
 // Date() without any parameter returns same result as Date.now()
 const date = new Date(Date());
@@ -24,3 +28,17 @@ app.use(errorHandler);
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port} at ${date.toString()}`);
 });
+
+const createRoles = async () => {
+  const roles = [
+    { id: 1, name: "user" },
+    { id: 2, name: "admin" },
+  ];
+
+  // Check if Roles table has records
+  // this ensures that we do not create default Roles every time server starts
+  const existingRoles = await Role.findAll();
+  if (!existingRoles) {
+    roles.forEach((role) => Role.create(role));
+  }
+};
